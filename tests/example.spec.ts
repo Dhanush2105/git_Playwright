b7161148e96d759d6007f5ae8440d9ad189adef1
dhanushkodi <dhanushkodi210501@gmail.com>
2026-03-31 10:34:00 +0530

    Tc004 got added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,10 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('playwright github', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
