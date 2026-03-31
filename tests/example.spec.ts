7ef6df11a06a10b46045ee3328f2feea164515fb
dhanushkodi <dhanushkodi210501@gmail.com>
2026-03-31 10:41:37 +0530

    Tc 004 got updated

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,7 +18,7 @@ test('get started link', async ({ page }) => {
 });
 
 test('playwright github', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('updated the url');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
